39df595330a1adec485dd8cda8d04377476daff0
Crowdbotics <team@crowdbotics.com>
2021-07-27 16:54:55 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "testsnehapp_4Identifier",
+  url: "https://testsnehapp_4Identifier.botics.co",
+  api: "https://testsnehapp_4Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
